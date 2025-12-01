de2bf56e03b5ca46e0010d09d454d1335e0d330c
JOSE ROBERTO LISBOA DA SILVA FILHO <0031432412016@lti.fatecso.edu.br>
2025-12-01 17:13:18 -0300

    Terceira versão Funcional!!!

--- MPP_MVC_Carousel/obj/Debug/net9.0/MPP_MVC_Carousel.AssemblyInfo.cs
+++ MPP_MVC_Carousel/obj/Debug/net9.0/MPP_MVC_Carousel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MPP_MVC_Carousel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b5c354dabb61165a749e59c9754d3a2d3a9ef5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cbee6cd793a8cb660267dd7dafaa77bf417c55")]
 [assembly: System.Reflection.AssemblyProductAttribute("MPP_MVC_Carousel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MPP_MVC_Carousel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
